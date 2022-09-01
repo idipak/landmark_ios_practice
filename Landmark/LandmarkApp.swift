403adfa5587876b9cd6c4c796d8033baa5e6779f
Dipak Prasad <dipak@thecodebucket.com>
2022-09-01 13:33:57 +0530

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Xcode on 01/09/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
